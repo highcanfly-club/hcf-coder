5006b42fdb2ee1429459a77f4f029caeaf7f66e6
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2023-05-25 09:38:50 +0200

    app name and telemetry

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,4 +9,4 @@ echo "mount root@$REMOTEHOST:$REMOTEDIR in /home/coder/workdir/"
 echo "root@$REMOTEHOST:$REMOTEDIR /home/coder/workdir/  fuse.sshfs noauto,x-systemd.automount,_netdev,users,idmap=user,AddressFamily=inet,StrictHostKeyChecking=accept-new,IdentityFile=$IDENTITY,port=$REMOTEPORT,allow_other,reconnect 0 0" >> /etc/fstab
 #sshfs -p $REMOTEPORT -o AddressFamily=inet,StrictHostKeyChecking=accept-new  root@$REMOTEHOST:$REMOTEDIR /home/coder/workdir/
 /bin/mount /home/coder/workdir/
-/usr/bin/entrypoint.sh --auth none --bind-addr 0.0.0.0:8080 .
+/usr/bin/entrypoint.sh --auth none --disable-telemetry --bind-addr 0.0.0.0:8080 --app-name "SCTG Code-Server" .
