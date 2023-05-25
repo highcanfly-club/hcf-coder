a5ff34c50a371a52ad4d65e58fd57d6efcf11db8
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2023-05-25 08:18:38 +0200

    use fstab rather than sshfs

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-/usr/bin/secret2sshkey --secret ssh-key-secret --ssh-dir /root/.ssh
+SECRET=$(/usr/bin/secret2sshkey --secret ssh-key-secret --ssh-dir /root/.ssh)
+echo $SECRET
+IDENTITY=`echo $SECRET | awk -F ' ' '{print $3}' | awk -F '=' '{print $2}'`
+echo "Using IDENTITY=$IDENTITY"
 chmod -R go-rwx /root/.ssh
-sshfs -p $REMOTEPORT -o AddressFamily=inet,StrictHostKeyChecking=accept-new  root@$REMOTEHOST:$REMOTEDIR /home/coder/workdir/
+echo "mount root@$REMOTEHOST:$REMOTEDIR in /home/coder/workdir/"
+echo "root@$REMOTEHOST:$REMOTEDIR /home/coder/workdir/  fuse.sshfs noauto,x-systemd.automount,_netdev,users,idmap=user,AddressFamily=inet,StrictHostKeyChecking=accept-new,IdentityFile=$IDENTITY,port=$REMOTEPORT,allow_other,reconnect 0 0" >> /etc/fstab
+#sshfs -p $REMOTEPORT -o AddressFamily=inet,StrictHostKeyChecking=accept-new  root@$REMOTEHOST:$REMOTEDIR /home/coder/workdir/
+/bin/mount /home/coder/workdir/
 /usr/bin/entrypoint.sh --auth none --bind-addr 0.0.0.0:8080 .
