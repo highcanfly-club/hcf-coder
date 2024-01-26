bfb7a12b55b064fc9604256321f698c946f88ec3
Ronan <ronan.le_meillat@highcanfly.club>
2024-01-26 09:08:54 +0100

    add comment

--- scripts/change-vsix-requirements.sh
+++ scripts/change-vsix-requirements.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# This bash script is used to update the vscode version requirement in the package.json file of a VSIX extension.
+# It starts by storing the current working directory in a variable _PWD.
+# Then it loops over all VSIX files in the ./bin directory that match the pattern GitHub.copilot*.vsix.
+# For each VSIX file, it generates a unique UUID and creates a temporary directory with this UUID.
+# It then unzips the VSIX file into this temporary directory and removes the original VSIX file.
+# It uses sed to replace the vscode version requirement in the package.json file of the extension with a new requirement (>=1.80.0).
+# The updated extension files are then zipped back into a VSIX file with the original name and placed back in the ./bin directory.
+# The script then changes back to the original working directory and removes the temporary directory.
+# This process is repeated for all matching VSIX files in the ./bin directory.
 _PWD=$(pwd)
 for EXTENSION in $(ls ./bin/GitHub.copilot*.vsix); do
     UUID=$(uuidgen)
